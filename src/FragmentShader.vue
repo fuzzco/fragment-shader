05f1f8ca951553bf91c19353a4fc31cb9468d951
Sander Moolin <sandermoolin@gmail.com>
2020-08-18 14:57:23 -0400

    wip trimming

--- src/FragmentShader.vue
+++ src/FragmentShader.vue
@@ -3,14 +3,53 @@
         <!-- the shader will go here -->
         <slot />
         <!-- and the result will be drawn here -->
-        <canvas class="canvas" ref="canvas" />
+        <full-canvas
+            class="canvas"
+            ref="canvas"
+            @canvas-ready="initializeCanvas"
+        />
     </div>
 </template>
 
 <script>
+import FullCanvas from '@fuzzco/full-canvas'
+import init from './gl/init'
+import { vertex, fragment } from './libs/defaultShaders'
+import buildShaders from './gl/buildShaders'
+import buildPlane from './gl/buildPlane'
+
+export default {
+    components: { 'full-canvas': FullCanvas },
+    data() {
+        return {
+            gl: null
+        }
+    },
+    methods: {
+        initializeCanvas(canvas) {
+            // we will:
+            // 1. initialize the canvas webgl context
+            // 2. build our shaders
+            // 3. render a plane where the fragment shader can live
+
+            // initialize the canvas, saving the gl context
+            const { gl } = init(canvas)
+            this.gl = gl
+
+            // build the desired shaders
+            const programInfo = buildShaders(this.gl, vertex, fragment)
+
+            buildPlane(gl, programInfo)
+        }
+    }
+}
+
+/*
 import snoise from './libs/snoise'
 import rotate2d from './libs/rotate2d'
 import phenom from './libs/phenom-px'
+
+
 
 export default {
     props: {
@@ -38,79 +77,80 @@ export default {
         }
     },
     mounted() {
-        // if (!window.shader) window.shader = 1
-        this.startTime = Date.now()
-
-        // get the HTML of the first element in the default slot
-        // to use as the shader
-        let fragment = this.$slots.default[0].elm.innerHTML
-
-        // include external functions
-        fragment = fragment.replace('#include <snoise>', snoise)
-        fragment = fragment.replace('#include <rotate2d>', rotate2d)
-
-        // include time and resolution as default uniforms
-        const startingUniforms = {
-            uTime: {
-                type: 'float',
-                value: 0.0
-            },
-            uResolution: {
-                type: 'vec2',
-                value: [this.$refs.canvas.width, this.$refs.canvas.height]
-            },
-            ...this.uniforms
-        }
-
-        // render function
-        const render = uniforms => {
-            if (uniforms.uTime) {
-                uniforms.uTime.value = Date.now() - this.startTime
-            }
-            if (
-                uniforms.uResolution &&
-                this &&
-                this.$refs &&
-                this.$refs.canvas
-            ) {
-                uniforms.uResolution.value = [
-                    this.$refs.canvas.width,
-                    this.$refs.canvas.height
-                ]
-            }
-
-            // update passed uniforms each frame
-            if (this.uniforms) {
-                Object.keys(this.uniforms)
-                    .filter(Boolean)
-                    .forEach(u => {
-                        uniforms[u].value = this.uniforms[u].value
-                    })
-            }
-
-            // allow custom render function to be passed as prop
-            if (this.render && this.alive) {
-                this.render(uniforms)
-            }
-        }
-
-        const Phenomenon = require('phenomenon').default
-
-        // kick loop
-        phenom(
-            Phenomenon,
-            fragment,
-            startingUniforms,
-            render,
-            this.$refs.canvas,
-            this.min,
-            this.max
-        )
+        // // if (!window.shader) window.shader = 1
+        // this.startTime = Date.now()
+        //
+        // // get the HTML of the first element in the default slot
+        // // to use as the shader
+        // let fragment = this.$slots.default[0].elm.innerHTML
+        //
+        // // include external functions
+        // fragment = fragment.replace('#include <snoise>', snoise)
+        // fragment = fragment.replace('#include <rotate2d>', rotate2d)
+        //
+        // // include time and resolution as default uniforms
+        // const startingUniforms = {
+        //     uTime: {
+        //         type: 'float',
+        //         value: 0.0
+        //     },
+        //     uResolution: {
+        //         type: 'vec2',
+        //         value: [this.$refs.canvas.width, this.$refs.canvas.height]
+        //     },
+        //     ...this.uniforms
+        // }
+        //
+        // // render function
+        // const render = uniforms => {
+        //     if (uniforms.uTime) {
+        //         uniforms.uTime.value = Date.now() - this.startTime
+        //     }
+        //     if (
+        //         uniforms.uResolution &&
+        //         this &&
+        //         this.$refs &&
+        //         this.$refs.canvas
+        //     ) {
+        //         uniforms.uResolution.value = [
+        //             this.$refs.canvas.width,
+        //             this.$refs.canvas.height
+        //         ]
+        //     }
+        //
+        //     // update passed uniforms each frame
+        //     if (this.uniforms) {
+        //         Object.keys(this.uniforms)
+        //             .filter(Boolean)
+        //             .forEach(u => {
+        //                 uniforms[u].value = this.uniforms[u].value
+        //             })
+        //     }
+        //
+        //     // allow custom render function to be passed as prop
+        //     if (this.render && this.alive) {
+        //         this.render(uniforms)
+        //     }
+        // }
+        //
+        // const Phenomenon = require('phenomenon').default
+        //
+        // // kick loop
+        // phenom(
+        //     Phenomenon,
+        //     fragment,
+        //     startingUniforms,
+        //     render,
+        //     this.$refs.canvas,
+        //     this.min,
+        //     this.max
+        // )
     },
     beforeDestroy() {
         this.alive = false
     }
 }
+*/
 </script>
 
 <style>
---
+++ src/libs/defaultShaders.js
@@ -0,0 +1,18 @@
+// Vertex shader program
+// https://developer.mozilla.org/en-US/docs/Web/API/WebGL_API/Tutorial/Adding_2D_content_to_a_WebGL_context
+export const vertex = `
+attribute vec4 aVertexPosition;
+
+uniform mat4 uModelViewMatrix;
+uniform mat4 uProjectionMatrix;
+
+void main() {
+  gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
+}
+`
+
+export const fragment = `
+void main() {
+  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+}
+`
